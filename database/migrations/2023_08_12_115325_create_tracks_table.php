e324622ef7be788f87399cbe39ff4ee022f0bdfa
Kari Laari <kari@karin-atk.fi>
2023-08-14 08:58:10 +0300

    Allow audio_url to be empty
    Plan is to set it at later stage

--- database/migrations/2023_08_12_115325_create_tracks_table.php
+++ database/migrations/2023_08_12_115325_create_tracks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tracks', function (Blueprint $table) {
             $table->id();
             $table->string('source_url');
-            $table->string('audio_url');
+            $table->string('audio_url')->nullable();
             $table->string('name')->nullable();
             $table->text('description')->nullable();
             $table->timestamps();
